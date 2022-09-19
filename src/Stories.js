52c678601e0854883707c8c3071c426ae892163d
nandasino <fernandamsino@gmail.com>
2022-09-18 22:16:05 -0300

    stories pronto

--- src/Stories.js
+++ src/Stories.js
@@ -1,81 +1,33 @@
-export default function Stories(){
+function Storie(props){
     return(
-        <div class="stories">
           <div class="story">
             <div class="imagem">
-              <img src="assets/img/9gag.svg" />
+              <img src= {props.imagem} />
             </div>
             <div class="usuario">
-              9gag
+              {props.usuario}
             </div>
           </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/meowed.svg" />
-            </div>
-            <div class="usuario">
-              meowed
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/barked.svg" />
-            </div>
-            <div class="usuario">
-              barked
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/nathanwpylestrangeplanet.svg" />
-            </div>
-            <div class="usuario">
-              nathanwpylestrangeplanet
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/wawawicomics.svg" />
-            </div>
-            <div class="usuario">
-              wawawicomics
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/respondeai.svg" />
-            </div>
-            <div class="usuario">
-              respondeai
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/filomoderna.svg" />
-            </div>
-            <div class="usuario">
-              filomoderna
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/memeriagourmet.svg" />
-            </div>
-            <div class="usuario">
-              memeriagourmet
-            </div>
-          </div>
-
-          <div class="setinha">
-            <ion-icon name="chevron-forward-circle"></ion-icon>
-          </div>
-        </div>
     );
 };
+
+export default function Stories(){
+  const lista =[
+    {imagem:"assets/img/9gag.svg",usuario:"9gag"},
+    {imagem:"assets/img/9gag.svg",usuario:"9gag"},
+    {imagem:"assets/img/9gag.svg",usuario:"9gag"},
+    {imagem:"assets/img/9gag.svg",usuario:"9gag"},
+    {imagem:"assets/img/9gag.svg",usuario:"9gag"},
+    {imagem:"assets/img/9gag.svg",usuario:"9gag"},
+    {imagem:"assets/img/9gag.svg",usuario:"9gag"},       
+  ];
+
+return(
+  <div class="stories">
+    {lista.map((s)=> <Storie imagem={s.imagem} usuario={s.usuario}/>)}
+    <div class="setinha">
+      <ion-icon name="chevron-forward-circle"></ion-icon>
+    </div>
+  </div>
+);
+}
