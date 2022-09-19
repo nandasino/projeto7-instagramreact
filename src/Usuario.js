e6f5e08b4410544432ed8588d94a798c25a6e901
nandasino <fernandamsino@gmail.com>
2022-09-18 23:44:22 -0300

    usuario pronto

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,14 +1,23 @@
+function Nome(props){
+  return(
+    <div class="usuario">
+    <img src= {props.imagem} />
+    <div class="texto">
+      <strong>{props.usuario}</strong>
+      <span>
+        {props.usuario}
+        <ion-icon name="pencil"></ion-icon>
+      </span>
+    </div>
+  </div>
+  )
+}
+
 export default function Usuario(){
+  const objeto=[
+    {imagem:"assets/img/catanacomics.svg", usuario:"catanacomics", nome:"Catana"}
+  ]
     return(
-        <div class="usuario">
-          <img src="assets/img/catanacomics.svg" />
-          <div class="texto">
-            <strong>catanacomics</strong>
-            <span>
-              Catana
-              <ion-icon name="pencil"></ion-icon>
-            </span>
-          </div>
-        </div>
+      objeto.map((u)=> <Nome imagem= {u.imagem} usuario= {u.usuario} nome={u.nome}/>)
     );
 };
