93a6cb3c0fd6ca7c60623de7db7ba374f7835d08
nandasino <fernandamsino@gmail.com>
2022-09-19 12:06:27 -0300

    alterar foto e usuário pronto

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,16 +1,32 @@
+import React from "react";
+
 function Nome(props){
+  const [nominho,setNominho] = React.useState("");
+  const [usuariozinho,setUsuariozinho] = React.useState("");
+  const [imagenzinha,setImagenzinha] = React.useState("");
+
+  function inserirNome(){
+    const nomePrompt= prompt("Qual é o seu nome?");
+    const usuarioPrompt= prompt("Qual é o seu usuário?")
+    setNominho(nomePrompt);
+    setUsuariozinho(usuarioPrompt);
+  };
+  function inserirImagem(){
+    const imagenzinhaPrompt= prompt("Qual é o link da imagem?");
+    setImagenzinha(imagenzinhaPrompt);
+  }
   return(
     <div class="usuario">
-    <img src= {props.imagem} />
+    <img onClick={inserirImagem}src= {(imagenzinha===""||imagenzinha===null)? props.imagem: imagenzinha}/>
     <div class="texto">
-      <strong>{props.usuario}</strong>
+      <strong>{(nominho===""||nominho===null||usuariozinho===""||usuariozinho===null)? props.usuario: usuariozinho}</strong>
       <span>
-        {props.usuario}
-        <ion-icon name="pencil"></ion-icon>
+        {(nominho===""||nominho===null||usuariozinho===""||usuariozinho===null)? props.nome: nominho}
+        <ion-icon name="pencil" onClick={inserirNome}></ion-icon>
       </span>
     </div>
   </div>
-  )
+  );
 }
 
 export default function Usuario(){
